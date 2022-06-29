9666a498442c5b441b974f9979b8d0dd2d586aa6
Juan Osorio <juancarlosocc@gmail.com>
2022-06-28 20:10:21 -0500

    Taller final de HTML5, CSS3 y JavaScript

---
+++ formulario.js
@@ -0,0 +1,11 @@
+function mostrar() {
+    var nombre = document.getElementById("nombre").value;
+    var correo = document.getElementById("correo").value;
+    var asunto = document.getElementById("asunto").value;
+    var mensaje = document.getElementById("mensaje").value;
+    console.log(nombre);
+    console.log(correo);
+    console.log(asunto);
+    console.log(mensaje);
+}
+
