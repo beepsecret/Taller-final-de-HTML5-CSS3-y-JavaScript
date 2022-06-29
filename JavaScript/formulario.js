069ff8a4e1b6d65a0a997ded4def3543f66f78a6
Juan Osorio <juancarlosocc@gmail.com>
2022-06-28 21:07:22 -0500

    Taller final de HTML5, CSS3 y JavaScript

--- JavaScript/formulario.js
+++ JavaScript/formulario.js
@@ -3,9 +3,29 @@ function mostrar() {
     var correo = document.getElementById("correo").value;
     var asunto = document.getElementById("asunto").value;
     var mensaje = document.getElementById("mensaje").value;
-    console.log(nombre);
-    console.log(correo);
-    console.log(asunto);
-    console.log(mensaje);
+    var alerta = "";
+    if (nombre.length != 0) {
+        console.log(nombre);
+    } else {
+        alerta += "\nEl nombre es obligatorio";
+    }
+    if (correo.length != 0) {
+        console.log(correo);
+    } else {
+        alerta += "\nEl correo es obligatorio";
+    }
+    if (asunto.length != 0) {
+        console.log(asunto);
+    } else {
+        alerta += "\nEl asunto es obligatorio";
+    }
+    if (mensaje.length != 0) {
+        console.log(mensaje);
+    } else {
+        alerta += "\nEl mensaje es obligatorio";
+    }
+    if (alerta.length != 0) {
+        alert(alerta);
+    }
 }
 
